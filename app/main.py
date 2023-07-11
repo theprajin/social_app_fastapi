833a14a861444822dc3f952bbf3a3226159ac0bc
theprajin <prazenthekiller@gmail.com>
2023-07-11 19:24:23 +0545

    pydantic response model added

--- app/main.py
+++ app/main.py
@@ -1,25 +1,18 @@
-from typing import Optional
+from typing import Optional, List
 from random import randrange
 import time
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body, Depends
-from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from . import models
+from . import models, schemas
+
 from sqlalchemy.orm import Session
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
 
 while True:
 
@@ -66,16 +59,16 @@ def root():
 #     return {"status": "success"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cur.execute("""SELECT * FROM posts""")
     # posts = cur.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post, db: Session = Depends(get_db)):  # title str, content str
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):  # title str, content str
     # cur.execute(
     #     """
     #     INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *
@@ -91,10 +84,10 @@ async def create_post(post: Post, db: Session = Depends(get_db)):  # title str, 
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cur.execute(
     #     """
@@ -112,7 +105,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id {id} was not found",
         )
-    return {"post_detail": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -133,15 +126,14 @@ def delete_post(id: int, db: Session = Depends(get_db)):
             detail=f"post with id: {id} does not exist",
         )
 
-
     post.delete(synchronize_session=False)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cur.execute(
     #     """
     #     UPDATE posts SET
@@ -164,4 +156,4 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
         )
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    return post_query.first()
---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+# class Post(BaseModel):
+#     title: str
+#     content: str
+#     published: bool = True
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
