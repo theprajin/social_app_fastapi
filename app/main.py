caaaa3b23a5c35ffa856b710f40c6d80f867b336
theprajin <prazenthekiller@gmail.com>
2023-07-13 16:42:21 +0545

    user authentication functionality added

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from . import models, schemas, utils
 
 from sqlalchemy.orm import Session
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -51,6 +51,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,11 +1,9 @@
-
 from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 
 from ..database import get_db
-from .. import models, schemas, utils
-
+from .. import models, schemas, utils, oauth2
 
 router = APIRouter(
     prefix="/posts",
@@ -14,7 +12,8 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db),
+              current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute("""SELECT * FROM posts""")
     # posts = cur.fetchall()
     posts = db.query(models.Post).all()
@@ -22,7 +21,9 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):  # title str, content str
+async def create_post(post: schemas.PostCreate,
+                      db: Session = Depends(get_db),
+                      current_user: int = Depends(oauth2.get_current_user)):  # title str, content str
     # cur.execute(
     #     """
     #     INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *
@@ -32,7 +33,7 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)): 
     #
     # new_post = cur.fetchone()
     # conn.commit()
-
+    print(current_user)
     new_post = models.Post(**post.dict())  # dict unpacking
     db.add(new_post)
     db.commit()
@@ -42,7 +43,8 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)): 
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db),
+             current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute(
     #     """
     #     SELECT * FROM posts WHERE id = %s
@@ -63,7 +65,8 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute(
     #     """
     #     DELETE FROM posts WHERE id = %s RETURNING *
@@ -87,7 +90,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute(
     #     """
     #     UPDATE posts SET
