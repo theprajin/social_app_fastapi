d2d30a3819a9f7fe4fe171d41f728bf2b2759fe0
theprajin <prazenthekiller@gmail.com>
2023-09-27 13:09:50 +0545

    env file added

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,18 +1,23 @@
+import os
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
 
 from . import schemas, database, models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+load_dotenv()
+
 # secret_key
 # algorithm
 # expiration time
 
-SECRET_KEY = "5joifjwojlkjdldf8sd57fs\d5f35ea4f6a46g84a4rg6846g8yh46sad4f68468fth4"
+# SECRET_KEY = "5joifjwojlkjdldf8sd57fs\d5f35ea4f6a46g84a4rg6846g8yh46sad4f68468fth4"
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
@@ -42,11 +47,13 @@ def verify_access_token(token: str, credentials_exception):
     return toke_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail=f"Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
+        headers={"WWW-Authenticate": "Bearer"},
     )
 
     token = verify_access_token(token, credentials_exception)
