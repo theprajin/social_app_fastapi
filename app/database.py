4cafab62fa6abbc34e390b146144d052ba242bcd
theprajin <prazenthekiller@gmail.com>
2023-09-27 13:31:36 +0545

    main.py file cleaned up

--- app/database.py
+++ app/database.py
@@ -1,10 +1,25 @@
+import time
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#this file contains database connection logics
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost/postgres'  # for SQLite, we need connect_args
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# this file contains database connection logics
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+HOST_NAME = os.getenv("HOST_NAME")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{HOST_NAME}/{DATABASE_NAME}"  # for SQLite, we need connect_args
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -21,4 +36,24 @@ def get_db():
     finally:
         db.close()
 
+
 # postgresql://<username>:<password>@<ip-address/hostname>/database_name
+
+
+# For reference only
+# while True:
+#     try:
+#         conn = psycopg2.connect(
+#             host="localhost",
+#             database="postgres",
+#             user="postgres",
+#             password="root",
+#             cursor_factory=RealDictCursor,
+#         )
+#         cur = conn.cursor()
+#         print("Database connection was successful")
+#         break
+#     except Exception as errors:
+#         print("Connecting to database failed")
+#         print("Error: ", errors)
+#         time.sleep(3)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,8 @@
-from typing import Optional, List
 from random import randrange
-import time
-from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body, Depends
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from fastapi import FastAPI
+from . import models
 
-from . import models, schemas, utils
 
-from sqlalchemy.orm import Session
 from .database import engine, get_db
 from .routers import post, user, auth
 
@@ -16,53 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost',
-                                database='postgres',
-                                user='postgres',
-                                password='root',
-                                cursor_factory=RealDictCursor)
-        cur = conn.cursor()
-        print("Database connection was successful")
-        break
-    except Exception as errors:
-        print("Connecting to database failed")
-        print("Error: ", errors)
-        time.sleep(3)
-
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "favorite food", "content": "I like pizza", "id": 2},
-]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-
-
-@app.get("/")
-def root():
-    return {"message": "This is your post"}
-
-
-
-
-
-
-
-
-
